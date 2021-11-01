592590594fab84636d503f12def11890e2330b37
mahfuz <mdmahfuzurrahmanarif@gmail.com>
2021-11-01 22:59:39 +0600

    optimized solution

--- noobSort_optimized.c
+++ noobSort_optimized.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 void noobSort(int *a, int n) {
 
   int *b = (int*)malloc(n * sizeof(int));
   int *mp = (int*)malloc(n * sizeof(int));
 
-  int i, j = 0, f_id = 0, r_id = n - 1;
+  int i, j = 0, f_id = -1, r_id = n;
   int cur_min, cur_max, min_loc, max_loc;
   int g_min = a[0], g_max = a[0];
 
+  // get the min and max elements of the initial array,
+  // we will use these value for further comparison
   for (i = 0; i < n; i++) {
     if (a[i] < g_min) {
       g_min = a[i];
@@ -20,11 +21,14 @@ void noobSort(int *a, int n) {
     }
   }
 
+  // run n / 2 iterations
   while (j < n / 2) {
     cur_min = g_max;
     cur_max = g_min;
 
+    // find min and max elements and their location
     for (i = 0; i < n; i++) {
+      // skip already marked indexes
       if (mp[i]) {
         continue;
       }
@@ -38,31 +42,32 @@ void noobSort(int *a, int n) {
       }
     }
 
-    b[f_id] = cur_min;
-    f_id++;
+    // push those min and max elements on appropriate position
+    // mark those indexes containing min and max elements
+    b[++f_id] = cur_min;
     mp[min_loc] = 1;
 
-    b[r_id] = cur_max;
-    r_id--;
+    b[--r_id] = cur_max;
     mp[max_loc] = 1;
 
     j++;
   }
+
   for (i = 0; i < n; i++) {
     if (mp[i] == 0) {
-      b[f_id] = a[i];
+      b[f_id + 1] = a[i];
       mp[i] = 1;
     }
-  }
-  for (i = 0; i < n; i++) {
     a[i] = b[i];
   }
+
   free(b);
+  free(mp);
 }
 
 int main() {
 
-  int arr[] = {9, 2, 11, 35, 1, 0, 44};
+  int arr[] = {9, 2, -11, 35, 1, 0, 44, 21, -4};
   int n = sizeof(arr) / sizeof(int), i;
 
   noobSort(arr, n);
